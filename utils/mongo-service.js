b1b8c5cf1ffe1d0dd3144c7601014168e88a33a8
Chaiyarin Niamsuwan <chaiyarin@odds.team>
2018-11-25 00:16:32 +0700

    insert locations to mongo when mongo not have locatoins data

--- utils/mongo-service.js
+++ utils/mongo-service.js
@@ -1,4 +1,5 @@
 var MongoClient = require('mongodb').MongoClient;
+var json_locations = require('../seed.json');
 var url = `mongodb://${process.env.SPYFALL_MONGO_USERNAME}:${process.env.SPYFALL_MONGO_PASSWORD}@${process.env.SPYFALL_MONGO_HOST}:${process.env.SPYFALL_MONGO_PORT}/${process.env.SPYFALL_MONGO_DB_NAME}`;
 
 module.exports = (async function() {
@@ -8,6 +9,18 @@ module.exports = (async function() {
   });
 
   const db = client.db('spyfall');
+
+  db.collection("locations").countDocuments(async function (err, count) {
+    if (!err && count === 0) {
+      await db.collection("locations").insertMany(json_locations, function(err,result) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log('Insert locations Success');
+        }
+      });
+    }
+  });
 
   const getLocations = await db.collection("locations").find({}).toArray();
 
